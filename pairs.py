515c68c2aa4ec069c2dc55efa667781665154770
gav <opi9a@gavinroberts.com>
2018-04-04 16:55:44 +0100

    update

--- pairs.py
+++ pairs.py
@@ -1,63 +1,103 @@
 import numpy as np
 
-def pair1(input_list, target=None, _debug=False):
+# set global default target
+glob_target = 10**9
 
-	if target is None:
-		target = int(len(input_list)*0.1)
+def pair1(input_list, target=glob_target, _debug=False):
 
-	pairs = []
+    pairs = []
 
-	# go thru each member of list
-	for i, num1 in enumerate(input_list):
-		if _debug: print("Trying input list index ", i, ": ", num1)
+    # go thru each member of list
+    for i, num1 in enumerate(input_list):
+        if _debug: print("Trying input list index ", i, ": ", num1)
 
-	# test with each other number (i.e. rest of list)
-		for j, num2 in enumerate(input_list[i+1:]):
+    # test with each other number (i.e. rest of list)
+        for j, num2 in enumerate(input_list[i+1:]):
 
-			if _debug: print("  with ", num2)
+            if _debug: print("  with ", num2)
 
-			if num1 + num2 == target:
-				print("FOUND PAIR: {}({}) + {}({}) = {}".format(num1, i, num2, j+i+1,
-																	 num1 + num2))
-				pairs.append((log[complement], i))
-				break
+            if num1 + num2 == target:
+                if _debug:
+                    print("FOUND PAIR: {}({}) + {}({}) = {}".format(num1,
+                                                            i, num2, j+i+1, num1 + num2))
+                pairs.append(max(num1, target-num1))
+                break
 
-	return pairs
+    return pairs
 
 
-def pair2(input_list, target=None, _debug=False):
+def pair1a(input_list, target=glob_target, _debug=False):
+    # sort list
+    # take first member
+    # add to last member, test
+    # repeat as long as sums are > target
 
-	if target is None:
-		target = int(len(input_list)*0.1)
-	
-	log = {}
-	pairs = []
+    pairs = []
 
-	# go thru each member of list
-	for i, num in enumerate(input_list):
-		if _debug: 
-			print("Trying input list index ", i, ": ", num)
-			print("log: ", [x for x in log.keys()])	
-	
-		# make complement and check if already seen
-		complement = target - num
-	
+    input_list = np.sort(input_list)
 
-		if log.get(complement, False):
-			print("FOUND PAIR: {}({}) + {}({}) = {}"
-						.format(complement, log[complement], num, i, num + complement))
+    # go thru each member of list
+    for i, num1 in enumerate(input_list):
+        if _debug: print("Trying input list index ", i, ": ", num1)
 
-			# add to results
-			pairs.append((log[complement], i))
+    # start from end and compare until sum is < target
+    # test with each other number (i.e. rest of list)
+        temp_sum = target + 1
+        ind = len(input_list) - 1
 
-		log[num] = i
+        while temp_sum >= target and ind > i:
 
-	return pairs
+            num2 = input_list[ind]
+            temp_sum = num1 + num2
+
+            if temp_sum == target:
+                if _debug:
+                    print("FOUND PAIR: {}({}) + {}({}) = {}".format(num1,
+                                                            i, num2, ind, temp_sum))
+                pairs.append(max(num1, target-num1))
+                break
+
+            if _debug:
+                print("temp sum is", temp_sum)
+                print("index ind is", ind)
+
+            ind -= 1
+
+    return pairs
+
+
+
+def pair2(input_list, target=glob_target, _debug=False):
+
+    log = {}
+    pairs = []
+
+    # go thru each member of list
+    for i, num in enumerate(input_list):
+        if _debug:
+            print("Trying input list index ", i, ": ", num)
+            print("log: ", [x for x in log.keys()])
+
+        # make complement and check if already seen
+        complement = target - num
+
+
+        if log.get(complement, False):
+            if _debug:
+                print("FOUND PAIR: {}({}) + {}({}) = {}"
+                        .format(complement, log[complement], num, i, num + complement))
+
+            # add to results
+            pairs.append(max(num, target-num))
+
+        log[num] = i
+
+    return pairs
 
 
 def make_test_list(scope, size=None, size_ratio=0.1):
 
-	if size == None:
-		size = int(scope*size_ratio)
+    if size == None:
+        size = int(scope*size_ratio)
 
-	return np.random.randint(0, high=scope, size=size)
+    return np.random.randint(0, high=scope, size=size)
